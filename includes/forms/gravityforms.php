47531d7da4055ec296003d1e9ca0b21afc5bfada
Vlad Olaru <vlad.olaru@pixelgrade.com>
2016-01-17 16:07:05 +0200

    improved gravityforms compatibility with any listing owner situation

--- includes/forms/gravityforms.php
+++ includes/forms/gravityforms.php
@@ -51,16 +51,18 @@ class Astoundify_Job_Manager_Contact_Listing_Form_GravityForms extends Astoundif
 
 		$notification[ 'toType' ] = 'email';
 
-		$listing = false;
+		$listing_ID = false;
 		$fields  = $form[ 'fields' ];
 
 		foreach ( $fields as $check ) {
-			if ( $check[ 'inputName' ] == 'Listing ID' ) {
-				$listing = $check[ 'id' ];
+			if ( $check[ 'label' ] == 'Listing ID' ) {
+				if ( isset( $entry[ $check[ 'id' ] ] ) ) {
+					$listing_ID = $entry[ $check['id'] ];
+				}
 			}
 		}
 
-		$object = get_post( $listing );
+		$object = get_post( $listing_ID );
 
 		if ( ! isset( $this->forms[ $object->post_type ] ) ) {
 			return;
@@ -71,6 +73,43 @@ class Astoundify_Job_Manager_Contact_Listing_Form_GravityForms extends Astoundif
 		}
 
 		$to = $object->_application ? $object->_application : $object->_candidate_email;
+
+		//if we couldn't find the email by now, get it from the listing owner
+		if ( empty( $to ) ) {
+			$owner_ID = false;
+
+			//first look at the metas
+			$meta = get_post_meta( $listing_ID );
+
+			/**
+			 * first check if this listing has been claimed by anybody
+			 * @link https://astoundify.com/downloads/wp-job-manager-claim-listing/
+			 */
+			$found_owner = false;
+			if ( isset( $meta['_claimed'] ) && ! empty( $meta['_claimed'] ) ) {
+				//get the first valid user ID that claimed the listing
+				foreach ( $meta['_claimed'] as $ID ) {
+					if ( ! empty( $ID ) ) {
+						$owner_ID = $ID;
+						$found_owner = true;
+						break;
+					}
+				}
+			}
+
+			if ( ! $found_owner ) {
+				//just get the email of the listing author if nothing else works
+				$owner_ID = $object->post_author;
+			}
+
+			//retrieve the owner user data to get the email
+			$owner_info = get_userdata( $owner_ID );
+
+			if ( false !== $owner_info ) {
+				$to = $owner_info->user_email;
+			}
+
+		}
 
 		$notification[ 'to' ] = $to;
 
